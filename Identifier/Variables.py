eaf805764659fee923cb76d73cab63e0adf8f2f6
gungun2233 <154960979+gungun2233@users.noreply.github.com>
2025-10-09 14:31:56 +0530

    Create Variables.py

---
+++ Identifier/Variables.py
@@ -0,0 +1,12 @@
+# Assign values
+length = 10       # 10 is variable and length is identifier.
+width = 5         # 5  is variable and width is identifier.
+
+# Use them in an expression
+area = length * width
+print(area)        # Output: 50
+
+# Change the value of a variable
+length = 20        # New value assigned to 'length'
+new_area = length * width
+print(new_area)    # Output: 100
