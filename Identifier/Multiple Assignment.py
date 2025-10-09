ee6c09126390b476ac53f658d916c7efc926a2e4
gungun2233 <154960979+gungun2233@users.noreply.github.com>
2025-10-09 14:33:12 +0530

    Create Multiple Assignment.py

---
+++ Identifier/Multiple Assignment.py
@@ -0,0 +1,7 @@
+# Assign multiple values to multiple variables
+a, b, c = 10, 20, 30
+print(a, b, c)      # Output: 10 20 30
+
+# Assign the same value to multiple variables
+x = y = z = 100
+print(x, y, z)      # Output: 100 100 100
