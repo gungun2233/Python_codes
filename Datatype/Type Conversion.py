786316dc35952fb8bf8f39503ef507544e6bbd6e
gungun2233 <154960979+gungun2233@users.noreply.github.com>
2025-10-13 12:03:11 +0530

    Create Type Conversion.py

---
+++ Datatype/Type Conversion.py
@@ -0,0 +1,9 @@
+# Explicit type conversion
+x = 5
+str_x = str(x)# Convert to string: "5"
+
+y = "10"
+int_y = int(y)# Convert to integer: 10
+
+z = 3.14
+int_z = int(z)# Convert to integer: 3 (truncates decimal)
