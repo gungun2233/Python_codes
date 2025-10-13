1f22a5b93d3b1969f610f1ec9205381c1bb56d70
gungun2233 <154960979+gungun2233@users.noreply.github.com>
2025-10-13 12:04:33 +0530

    Create Numeric Types.py

---
+++ Datatype/Numeric Types.py
@@ -0,0 +1,12 @@
+a = 10        # int
+b = 3.14      # float
+c = 2 + 3j    # complex
+
+print(type(a))  # <class 'int'>
+print(type(b))  # <class 'float'>
+print(type(c))  # <class 'complex'>
+
+# Operations
+print(a + b)     # 13.14
+print(c.real)    # 2.0
+print(c.imag)
