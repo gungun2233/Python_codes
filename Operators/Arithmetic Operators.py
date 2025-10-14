e5e7be9ed50bb602d6d45b919d9453d33566c81e
gungun2233 <154960979+gungun2233@users.noreply.github.com>
2025-10-14 11:12:03 +0530

    Create Arithmetic Operators.py

---
+++ Operators/Arithmetic Operators.py
@@ -0,0 +1,26 @@
+x=4
+y=3
+print(x+y) # 7
+print(x-y) # 1
+print(x*y) # 12
+print(x/y) # 1.3333333333
+print(x//y) # 1
+print(x%y) # 1
+print(x**y) # 64
+
+print("5 + 3 =", 5 + 3)      # Addition
+print("5 - 3 =", 5 - 3)      # Subtraction
+print("5 * 3 =", 5 * 3)      # Multiplication
+print("5 / 2 =", 5 / 2)      # Division (gives float)
+print("5 // 2 =", 5 // 2)    # Floor Division (integer result)
+print("5 % 2 =", 5 % 2)      # Modulus (remainder)
+print("2 ** 3 =", 2 ** 3)    # Exponentiation (power)
+
+Output:
+5 + 3 = 8
+5 - 3 = 2
+5 * 3 = 15
+5 / 2 = 2.5
+5 // 2 = 2
+5 % 2 = 1
+2 ** 3 = 8
