6e5c619b9dc8f365f3922a303d4fb959f36f076f
gungun2233 <154960979+gungun2233@users.noreply.github.com>
2025-10-28 09:34:13 +0530

    Assignment Operators code

---
+++ Operators/Assignment Operators.py
@@ -0,0 +1,31 @@
+# Basic Assignment
+x = 10   # Assign 10 to x
+print("x =", x)  # 10
+
+# Add AND assign
+x += 5   # x = x + 5
+print("x += 5 ->", x)  # 15
+
+# Subtract AND assign
+x -= 3   # x = x - 3
+print("x -= 3 ->", x)  # 12
+
+# Multiply AND assign
+x *= 2   # x = x * 2
+print("x *= 2 ->", x)  # 24
+
+# Divide AND assign
+x /= 4   # x = x / 4
+print("x /= 4 ->", x)  # 6.0
+
+# Modulus AND assign
+x %= 4   # x = x % 4
+print("x %= 4 ->", x)  # 2.0
+
+# Exponent AND assign
+x **= 3  # x = x ** 3
+print("x **= 3 ->", x)  # 8.0
+
+# Floor Division AND assign
+x //= 2  # x = x // 2
+print("x //= 2 ->", x)  # 4.0
