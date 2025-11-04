2ff90dd187592eba2a62c6852b0e65657d23ff36
gungun2233 <154960979+gungun2233@users.noreply.github.com>
2025-11-04 10:22:19 +0530

    Add membership operators examples for strings
    Demonstrate membership operators with string examples.

---
+++ Operators/Membership Operators.py
@@ -0,0 +1,7 @@
+# For Strings
+
+text = "welcome to agra"
+print("agra" in text)       #  True (substring found)
+print("Agra" in text)       #  False (case-sensitive)
+print("come" not in text)   #  False (since "come" is present)
+
