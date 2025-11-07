40fce5a0aeaea8d6ffce5c116272448df516e3a9
gungun2233 <154960979+gungun2233@users.noreply.github.com>
2025-11-07 09:38:20 +0530

    Add membership operator examples for tuples
    Added examples of membership operators for tuples.

--- Operators/Membership Operators.py
+++ Operators/Membership Operators.py
@@ -12,3 +12,10 @@ print("agra" in cities)     #  True (element present)
 print("goa" not in cities)  #  True (element not present)
 print("a" in cities)        #  False (not element, just part of string)
 
+# For Tuples
+
+numbers = (1, 2, 3, 4)
+print(2 in numbers)         # True
+print(5 not in numbers)     #  True
+
+
